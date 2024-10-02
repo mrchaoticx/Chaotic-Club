bb960ec441ef94c13fa02a31d92ff8a8ecb70ba8
Mr Chaotic <westberginc1@gmail.com>
2024-10-02 16:25:59 -0500

    Update api.js
    Add data for future changes

--- scripts/api.js
+++ scripts/api.js
@@ -1,5 +1,6 @@
 const data = {
-    "pre": "https://mrchaoticx.github.io/Chaotic-Club/articles/"
+    "pre": "https://mrchaoticx.github.io/Chaotic-Club/articles/",
+    "cid": "content"
 }
 
 const queryString = window.location.search;
@@ -12,6 +13,6 @@ fetch(pre)
   .then(response => response.text())
   .then(content => {
     console.log(content);
-    const contentID = document.getElementById("content");
+    const contentID = document.getElementById(data.cid);
     contentID.innerHTML = marked.parse(content);
   });
