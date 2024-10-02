835bef409f8ae55caebd5db35044778acf00915a
Mr Chaotic <westberginc1@gmail.com>
2024-10-01 20:48:47 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,17 @@
+const data = {
+    "pre": "https://mrchaoticx.github.io/Chaotic-Club/articles/"
+}
 
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+let article = urlParams.get('key');
+let pre = `${data.pre}${article}`;
+
+fetch(pre)
+  .then(response => response.text())
+  .then(content => {
+    console.log(content);
+    const contentID = document.getElementById("content");
+    contentID.innerHTML = marked.parse(content);
+  });
