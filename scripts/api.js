38190047cb9b9dd765b6d880690d1ef926cbff26
Mr Chaotic <westberginc1@gmail.com>
2024-10-05 17:28:06 -0500

    Update api.js
    Lots of changes revolving around Fetch and InnerHTML errors.

--- scripts/api.js
+++ scripts/api.js
@@ -1,18 +1,33 @@
-const data = {
+addEventListener("DOMContentLoaded", (event) => {
+  const data = {
     "pre": "https://mrchaoticx.github.io/Chaotic-Club/articles/",
     "cid": "content"
-}
+  };
 
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const contentID = document.getElementById(data.cid);
 
-let article = urlParams.get('key');
-let pre = `${data.pre}${article}`;
+  if (!contentID) {
+    console.error(`Element with ID ${data.cid} not found.`);
+  } else {
+    let article = urlParams.get('key');
+    let pre = `${data.pre}${article}`;
 
-fetch(pre)
-  .then(response => response.text())
-  .then(content => {
-    console.log(content);
-    const contentID = document.getElementById(data.cid);
-    contentID.innerHTML = marked.parse(content);
-  });
+    fetch(pre)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.text();
+      })
+      .then(content => {
+        console.log("Res:", content);
+        contentID.innerHTML = marked.parse(content);
+      })
+      .catch(error => {
+        console.error("Error fetching article:", error);
+        contentID.innerHTML = `<h1>${error.message}</h1><p></p>`;
+      });
+  }
+});
