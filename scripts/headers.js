c9026c5f5fbeaef18bf8b6fd066ca081718bca01
Mr Chaotic <westberginc1@gmail.com>
2024-10-05 21:04:37 -0500

    Create headers.js

---
+++ scripts/headers.js
@@ -0,0 +1,20 @@
+const data = {
+    "tag": "h2",
+    "hid": "headers"
+};
+
+function headersCalled() {
+    return new Promise((resolve) => {
+        document.addEventListener('apiContentLoaded', () => {
+            resolve();
+        });
+    });
+}
+
+headersCalled().then(() => {
+    const headerID = document.getElementById(data.hid);
+    const h2Elements = document.querySelectorAll(`h2`);
+    const h2Texts = Array.from(h2Elements).map(h2 => h2.textContent);
+    const h2Html = h2Texts.map(text => `<p>${text}</p>`).join('');
+    headerID.innerHTML = h2Html;
+});
