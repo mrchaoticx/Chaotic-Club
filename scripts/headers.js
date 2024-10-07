2ec10b4be8c6e6d1b273b3f8f9509fa5f4ca80e2
Mr Chaotic <westberginc1@gmail.com>
2024-10-06 19:39:14 -0500

    Update headers.js

--- scripts/headers.js
+++ scripts/headers.js
@@ -1,20 +1,20 @@
-const data = {
+const data2 = {
     "tag": "h2",
     "hid": "headers"
 };
 
 function headersCalled() {
     return new Promise((resolve) => {
-        document.addEventListener('apiContentLoaded', () => {
-            resolve();
-        });
+      document.addEventListener('apiContentLoaded', () => {
+        resolve();
+      });
     });
 }
-
-headersCalled().then(() => {
-    const headerID = document.getElementById(data.hid);
+  
+ headersCalled().then(() => {
+    const headerID = document.getElementById(data2.hid); 
     const h2Elements = document.querySelectorAll(`h2`);
     const h2Texts = Array.from(h2Elements).map(h2 => h2.textContent);
-    const h2Html = h2Texts.map(text => `<a href=${null}>${text}</a><br>`).join('');
+    const h2Html = h2Texts.map(text => `<a href="#${text}">${text}</a><br>`).join('');
     headerID.innerHTML = h2Html;
 });
