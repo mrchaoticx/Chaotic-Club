fadc95246d1c07b242537806e45a794378d8cf36
Mr Chaotic <westberginc1@gmail.com>
2024-10-06 19:27:08 -0500

    Update headers.js

--- scripts/headers.js
+++ scripts/headers.js
@@ -15,6 +15,6 @@ headersCalled().then(() => {
     const headerID = document.getElementById(data.hid);
     const h2Elements = document.querySelectorAll(`h2`);
     const h2Texts = Array.from(h2Elements).map(h2 => h2.textContent);
-    const h2Html = h2Texts.map(text => `<p>${text}</p>`).join('');
+    const h2Html = h2Texts.map(text => `<a href=${null}>${text}</a><br>`).join('');
     headerID.innerHTML = h2Html;
 });
